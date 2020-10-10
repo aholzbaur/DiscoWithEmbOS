7f28ffe0760707b3cf4e57faa3871fdf3ec73f1e
aholzbaur <71641117+aholzbaur@users.noreply.github.com>
2020-10-10 16:27:32 +0200

    Added minimal version

---
+++ port/startup/system_stm32f4xx.h
@@ -0,0 +1,12 @@
+#ifndef __SYSTEM_STM32F4XX_H
+#define __SYSTEM_STM32F4XX_H
+
+extern uint32_t SystemCoreClock;          /*!< System Clock Frequency (Core Clock) */
+
+extern const uint8_t  AHBPrescTable[16];    /*!< AHB prescalers table values */
+extern const uint8_t  APBPrescTable[8];     /*!< APB prescalers table values */
+  
+extern void SystemInit(void);
+extern void SystemCoreClockUpdate(void);
+
+#endif /*__SYSTEM_STM32F4XX_H */
